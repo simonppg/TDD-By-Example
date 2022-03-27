9b8ba29665937bd1f2f3c3667784c24121f6c6ba
simonppg <simonppg@gmail.com>
2022-03-26 19:18:34 -0700

    First failing test

---
+++ README.md
@@ -0,0 +1,9 @@
+# TDD By Example
+
+This is a TDD exercise from the book Test-Driven Development By Example By Kent Beck
+
+## Running the tests
+
+```shell
+./gradlew clean test
+```
---
+++ src/test/java/com/tddbyexample/money/MoneyTest.java
@@ -0,0 +1,14 @@
+package com.tddbyexample.money;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+  @Test
+  public void firstTest() {
+    fail("bad test");
+    
+  }
+  
+}
